a447e94bce8d32b98ae181248e94056c81752c9c
Matheus Pires <matheushenriquepires99@gmail.com>
2020-07-20 14:46:20 -0300

    Session and forgot password

---
+++ app/Controllers/Http/ForgotPasswordController.js
@@ -0,0 +1,22 @@
+'use strict'
+
+const crypto = require('crypto');
+const User = use('App/Models/User');
+
+class ForgotPasswordController {
+  async store({ request, response }) {
+    const email = request.input('email');
+    const user = await User.findBy('email', email);
+
+    if (!user) {
+      return response.status(400).send({ error: { message: 'User not found' } });
+    }
+
+    user.token = crypto.randomBytes(10).toString('hex');
+    user.token_created_at = new Date();
+
+    await user.save();
+  }
+}
+
+module.exports = ForgotPasswordController
